aa576b9f658762017cb850ee1a9b654f5473872d
Basavesh A S <basavesh.logan@gmail.com>
2013-07-12 21:53:58 +0530

    Floodlight initial

--- Floodlight_topo.py
+++ Floodlight_topo.py
@@ -11,7 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+'''
+This script collects Topology information from Floodlight controller 
+using REST API
+'''
 
 import os 				# OS Calls
 import sys 				# System Calls
