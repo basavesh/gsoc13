df1d75c048e519418173d0869ca90041a711fe1d
Basavesh A S <basavesh.logan@gmail.com>
2013-08-28 03:05:27 +0530

    enabled json dumping

--- Floodlight_topo.py
+++ Floodlight_topo.py
@@ -25,6 +25,8 @@ import sys 				# System Calls
 import json				# To convert to and fro from json to python objects
 import struct 
 import requests
+import pickle
+import getopt
 
 controllerIP = 'localhost'		 # 
 cport = '8080'
@@ -88,140 +90,128 @@ class MyTopology (object):
 
 	def add_link(self, dpid1, port1, dpid2, port2):
 	
-		if (dpid2,dpid1) not in self.links:
-			self.links[(dpid1,dpid2)] = {}
-			self.links[(dpid1,dpid2)]['src_port'] = port1
-			self.links[(dpid1,dpid2)]['dst_port'] = port2
+		if str(dpid2) + ' ' + str(dpid1) not in self.links:
+
+			link =  str(dpid1) + ' ' + str(dpid2)
+
+			self.links[link] = {}
+			self.links[link]['src_port'] = port1
+			self.links[link]['dst_port'] = port2
 		
 
-	def del_link(self, dpid1, dpid2):
+def main(): 		
 
-		del self.links[(dpid1,dpid2)]
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "o:", ["output="])
+	except getopt.GetoptError as err:
+		print str(err) # will print something like "option -a not recognized"
+		sys.exit(2)
+
+	outfile = 'jsondata.txt'
+
+	for o, a in opts:
+            
+		if o in ("-o", "--output"):
+			outfile = a
+
+		else:
+			assert False, "unhandled option"
+
+	topology = MyTopology() # Created an empty instance of Mytopology 
 
 
+	##### Fetch data From Controller using REST API ############################
+
+	######################## get list of all switches ##########################
+	try:
+		command = 'http://'+controllerIP+ \
+		          ':'+cport+'/wm/core/controller/switches/json'
+
+	except Exception:
+		print "make sure that the controller is running"
+		sys.exit()
+
+	r = requests.get(command)
+	switches = json.loads(r.content)
+
+	for i in range(len(switches)):
+		switch_dpid = switches[i]['dpid']
+		topology.add_switch(switch_dpid)
+
+	print "\nSwitches are: "
+	for switch in topology.switches:
 		
+		print topology.switches[switch]['name'], switch
 
-topology = MyTopology() # Created an empty instance of Mytopology 
+	#################################end #######################################
 
+	####################### get list of end devices ############################
+	command = 'http://'+controllerIP+ ':'+cport+'/wm/device/'
+	r = requests.get(command)
+	hosts = json.loads(r.content)
+	for i in range(len(hosts)):
+		if len(hosts[i]['attachmentPoint']) > 0:
+			
+			#ipv4 = hosts[i]['ipv4'][0]
+			mac = hosts[i]['mac'][0]
+			to_switch = hosts[i]['attachmentPoint'][0]['switchDPID']
+			to_port = hosts[i]['attachmentPoint'][0]['port']
 
-##### Fetch data From Controller using REST API ############################
+			topology.add_host(mac, None, to_switch, to_port)
 
-######################## get list of all switches ##########################
-try:
-	command = 'http://'+controllerIP+ \
-	          ':'+cport+'/wm/core/controller/switches/json'
+			try:
+				if hosts[i]['ipv4'][0]:
+					topology.update_IP(mac,hosts[i]['ipv4'][0])
 
-except Exception:
-	print "make sure that the controller is running"
-	sys.exit()
+			except Exception :
+				pass
+					
+	print "\nHosts are: "
+	for host in topology.hosts:
+		print "Name: {}".format(topology.hosts[host]['name'])
+		print "MAC: {}".format(host)
+		print "IP: {}".format(topology.hosts[host]['IP'])
+		print "to_switch: {}".format(topology.hosts[host]['to_switch'])
+		print "to_port: {}".format(topology.hosts[host]['to_port'])
+		print "\n"
 
-r = requests.get(command)
-switches = json.loads(r.content)
-
-for i in range(len(switches)):
-	switch_dpid = switches[i]['dpid']
-	topology.add_switch(switch_dpid)
-
-print "\nSwitches are: "
-for switch in topology.switches:
-	
-	print topology.switches[switch]['name'], switch
-
-#################################end #######################################
-
-####################### get list of end devices ############################
-command = 'http://'+controllerIP+ ':'+cport+'/wm/device/'
-r = requests.get(command)
-hosts = json.loads(r.content)
-for i in range(len(hosts)):
-	if len(hosts[i]['attachmentPoint']) > 0:
-		
-		#ipv4 = hosts[i]['ipv4'][0]
-		mac = hosts[i]['mac'][0]
-		to_switch = hosts[i]['attachmentPoint'][0]['switchDPID']
-		to_port = hosts[i]['attachmentPoint'][0]['port']
-
-		topology.add_host(mac, None, to_switch, to_port)
-
-		try:
-			if hosts[i]['ipv4'][0]:
-				topology.update_IP(mac,hosts[i]['ipv4'][0])
-
-		except Exception :
-			pass
-				
-print "\nHosts are: "
-for host in topology.hosts:
-	print "Name: {}".format(topology.hosts[host]['name'])
-	print "MAC: {}".format(host)
-	print "IP: {}".format(topology.hosts[host]['IP'])
-	print "to_switch: {}".format(topology.hosts[host]['to_switch'])
-	print "to_port: {}".format(topology.hosts[host]['to_port'])
-	print "\n"
-
-#for host in topology.hosts:
-#	print host	
-################################end ########################################
+	#for host in topology.hosts:
+	#	print host	
+	################################end ########################################
 
 
-################## get list of links #######################################
-command = 'http://'+controllerIP+ ':'+cport+'/wm/topology/links/json'
-r = requests.get(command)
-links = json.loads(r.content)
+	################## get list of links #######################################
+	command = 'http://'+controllerIP+ ':'+cport+'/wm/topology/links/json'
+	r = requests.get(command)
+	links = json.loads(r.content)
 
-for i in range(len(links)):
-	src_switch = links[i]['src-switch']
-	dst_switch = links[i]['dst-switch']
-	src_port = links[i]['src-port']
-	dst_port = links[i]['dst-port']
-	topology.add_link(src_switch, src_port, dst_switch, dst_port)
+	for i in range(len(links)):
+		src_switch = links[i]['src-switch']
+		dst_switch = links[i]['dst-switch']
+		src_port = links[i]['src-port']
+		dst_port = links[i]['dst-port']
+		topology.add_link(src_switch, src_port, dst_switch, dst_port)
 
-for link in topology.links:
-	print "{} --> {} {}".format(link[0],link[1],topology.links[link])
-############################# end ##########################################
+
+	# editted hereeeeeee --- 
+
+
+	for link in topology.links:
+		print "{} --> {} {}".format(link.split(' ')[0],link.split(' ')[1],topology.links[link])
+	############################# end ##########################################
 
 
 
+	############## try for json formatting ################
+	data = {}
+	data['switches'] = topology.switches
+	data['hosts'] = topology.hosts
+	data['links'] = topology.links
+
+	f = open(outfile,'w')
+	json.dump(data, f)
+	f.close()
 
 
-# try to create a simple mininet topo file
-
-f = open('test.py', 'w')
-
-f.write("from mininet.topo import Topo\n\n")
-f.write("class MyTopo( Topo ):\n")
-f.write("\t'Trying to create a Mininet File'\n")
-
-f.write("\n\tdef __init__( self ):\n")
-
-
-f.write("\n\t\tTopo.__init__( self )\n")
-f.write("\t\t# Initialize topology\n\n")
-
-f.write("\t\t# Add hosts\n")
-for host in topology.hosts:
-	if topology.hosts[host]['name'] is None:
-		f.write("\t\t{} = self.addHost('{}', mac = '{}')\n".format(topology.hosts[host]['name'],topology.hosts[host]['name'], host))
-
-	else:
-		f.write("\t\t{} = self.addHost('{}', ip = '{}', mac = '{}' )\n".format(topology.hosts[host]['name'],topology.hosts[host]['name'], topology.hosts[host]['IP'], host))
-
-
-f.write("\n")
-
-f.write("\t\t# Add switches\n")
-for switch in topology.switches:
-	f.write("\t\t{} = self.addSwitch('{}')\n".format(topology.switches[switch]['name'],topology.switches[switch]['name']))
-
-f.write("\n\t\t# Add links\n")
-for link in topology.links:
-	f.write("\t\tself.addLink( {}, {}, {}, {} )\n".format(topology.switches[link[0]]['name'],topology.switches[link[1]]['name'], topology.links[link]['src_port'], topology.links[link]['dst_port'] ))
-
-f.write("\n\n")
-for host in topology.hosts:
-	f.write("\t\tself.addLink( {}, {}, 1, {} )\n".format(topology.hosts[host]['name'], topology.switches[topology.hosts[host]['to_switch']]['name'], topology.hosts[host]['to_port']))
-
-
-f.write("\n\ntopos = { 'mytopo': ( lambda: MyTopo() ) }")
-f.close()
-
+if __name__ == "__main__":
+    main()
